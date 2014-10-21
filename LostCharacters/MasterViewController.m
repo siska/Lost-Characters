8b93585481cfbdf4814e729a1b746ac12f4ada03
Ryan Siska <rsiska1@gmail.com>
2014-10-21 14:36:31 -0500

    In process of creating pullFromPList - will bring in the plist data, convert it to a Character entity that I'll create in the xcdatamodeld, and then put the character objects into the array

--- LostCharacters/MasterViewController.m
+++ LostCharacters/MasterViewController.m
@@ -20,6 +20,68 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+    if (self.characters.count == 0) {
+        [self pullFromPList];
+    }
+    else
+    {
+        //will be loadData
+    }
+
+}
+
+-(void)pullFromPList
+{
+    NSURL *url = [NSURL URLWithString:@"http://s3.amazonaws.com/mobile-makers-assets/app/public/ckeditor_assets/attachments/2/lost.plist"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError)
+    {
+
+        {
+            NSError *connectionError = nil;
+            if (data)
+            {
+                NSDictionary *dict = [NSPropertyListSerialization propertyListWithData:data options:NSPropertyListReadCorruptError format:NULL error:&connectionError];
+                NSArray *tempDataArray = [dict objectForKey:@"Root"];
+
+                NSLog(@"%@", tempDataArray);
+
+
+
+
+            }
+        }
+
+    /*
+        {
+            NSError *jsonError = nil;
+            if (data)
+            {
+                NSDictionary *tempDictionary = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
+                NSArray *tempDataArray = [tempDictionary objectForKey:@"data"];
+
+                for (NSDictionary *tempDictionary in tempDataArray) //to be able to pull objectForKeys from Dictionary
+                {
+                    if (self.photos.count < 10)
+                    {
+                        NSDictionary *tempImagesDictionary = [tempDictionary objectForKey:@"images"];
+                        NSDictionary *tempLowResDictionary = [tempImagesDictionary objectForKey:@"low_resolution"];
+                        NSString *tempURLString = [tempLowResDictionary objectForKey:@"url"];
+
+                        PhotoInfo *photo = [[PhotoInfo alloc] initWithImage:tempURLString]; //learned this - taylors meetup project implementation
+                        [self.photos addObject:photo];
+                    }
+                }
+                NSLog(@"self.photos %@", self.photos);
+                [self.imageCollectionView reloadData];
+                [self.imageCollectionView setContentOffset:CGPointZero animated:YES];
+            }
+            NSLog(@"Connection Error: %@", connectionError);
+            NSLog(@"JSON Error: %@", jsonError);
+
+
+        NSLog(@"%@", dict);
+    }];*/
 }
 
 @end
