03229b7068c59ff6644825216abe8269cbce500a
Ryan Siska <rsiska1@gmail.com>
2014-10-21 21:18:34 -0500

    Added the delegate methods in AddCharVC.m - self.actorTextField.delegate = self and the other one

--- LostCharacters/AddCharacterViewController.m
+++ LostCharacters/AddCharacterViewController.m
@@ -16,18 +16,30 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.actorTextField.delegate = self;
+    self.passengerTextField.delegate = self;
     // Do any additional setup after loading the view.
 }
+
+-(void)textFieldDidBeginEditing:(UITextField *)textField
+{
+    [textField becomeFirstResponder];
+}
+
 
 -(BOOL)textFieldShouldReturn:(UITextField *)textField
 {
     if (textField.tag == 1)
     {
         self.actorTextField.text = textField.text;
+        [textField resignFirstResponder];
+        NSLog(@"actor: %@", self.actorTextField.text);
     }
     else if (textField.tag == 2)
     {
         self.passengerTextField.text = textField.text;
+        [textField resignFirstResponder];
+        NSLog(@"passenger: %@", self.passengerTextField.text);
     }
     return YES;
 }
