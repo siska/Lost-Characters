e5d8503ae72cb5cb163287e9a4e28b98bf9e62fd
Ryan Siska <rsiska1@gmail.com>
2014-10-21 19:23:08 -0500

    Created file for new VC of type UIViewController - set segue from add button to new AddCharacterVC

---
+++ LostCharacters/AddCharacterViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddCharacterViewController.h
+//  LostCharacters
+//
+//  Created by S on 10/21/14.
+//  Copyright (c) 2014 Ryan Siska. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddCharacterViewController : UIViewController
+
+@end
---
+++ LostCharacters/AddCharacterViewController.m
@@ -0,0 +1,37 @@
+//
+//  AddCharacterViewController.m
+//  LostCharacters
+//
+//  Created by S on 10/21/14.
+//  Copyright (c) 2014 Ryan Siska. All rights reserved.
+//
+
+#import "AddCharacterViewController.h"
+
+@interface AddCharacterViewController ()
+
+@end
+
+@implementation AddCharacterViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
